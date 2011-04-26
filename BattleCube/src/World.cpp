567c8469862db91737c6dbe3c75f8dafbe1dd337
Dave Hoatlin <dhoatlin@gmail.com>
2011-04-25 20:54:43 -0400

    added texture images, trying to render plane textures

--- BattleCube/src/World.cpp
+++ BattleCube/src/World.cpp
@@ -1,7 +1,8 @@
 #include "../include/World.h"
 
 using namespace std;
-
+GLubyte wallTexture[340][340][3];
+GLubyte groundTexture[340][340][3];
 
 World::World()
 {
@@ -31,12 +32,44 @@ void World::NewBullet() {
 }
 
 void World::SetTexture(){
+    int i, j;
+    unsigned char data[3];
+    FILE *fp;
+    char file[256] = "wall.dat";
+    char file2[256] = "ground.dat";
+    if((fp = fopen(file, "rb")) == NULL) {
+        printf("file not found");
+        exit(0);
+    }
 
+    for(i = 0; i < 340; i++){
+        for(j = 0; j < 340; j++){
+            fread(data, sizeof(unsigned char), 3, fp);
+            wallTexture[i][j][0] = (GLubyte) data[0];
+            wallTexture[i][j][1] = (GLubyte) data[1];
+            wallTexture[i][j][2] = (GLubyte) data[2];
+        }
+    }
+
+    if((fp = fopen(file2, "rb")) == NULL) {
+        printf("file not found");
+        exit(0);
+    }
+    for(i = 0; i < 340; i++){
+        for(j = 0; j < 340; j++){
+            fread(data, sizeof(unsigned char), 3, fp);
+            groundTexture[i][j][0] = (GLubyte) data[0];
+            groundTexture[i][j][1] = (GLubyte) data[1];
+            groundTexture[i][j][2] = (GLubyte) data[2];
+        }
+    }
 }
 
 void World::Draw() {
 
     bool collision = false;
+
+
 
     glColor3f(0.5, 0.5, 0.5);
     GLfloat size = World::m_WallLength;
@@ -44,23 +77,38 @@ void World::Draw() {
 
     glColor3f(0.1, 0.1, 0.1);
     glPushMatrix();
-        glTranslatef(0.0, FloorPos(), 0.0);
+        glBegin(GL_QUADS);
+            glVertex3f(-pos, FloorPos(), -pos);
+            glVertex3f(pos, FloorPos(), -pos);
+            glVertex3f(pos, FloorPos(), pos);
+            glVertex3f(-pos, FloorPos(), pos);
+        glEnd();
+    glPopMatrix();
+
+    /*
+    glPushMatrix();
+        glTransaltef(0.0, FloorPos(), 0.0);
         glScalef(1.0, 0.001, 1.0);
         glutSolidCube(size);
     glPopMatrix();
-/*
+    */
+
     glPushMatrix();
-        glBegin(GL_QUADS);
-            glTexImage2d(GL_TEXTURE_2D, 0, GL_RGB, 340, 340, 0, GL_RGB, GL_UNSIGNED_BYTE, wallTexture);
+        glBegin(GL_POLYGON);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 340, 340, 0, GL_RGB, GL_UNSIGNED_BYTE, wallTexture);
             glEnable(GL_TEXTURE_2D);
             glTexCoord2f(0.0, 0.0);
-            glVertex3f()
-            glTexCoord2f(0.0, 1.0);
-            glTexCoord2f(1.0, 1.0);
+            glVertex3f(-pos, 50, -pos);
             glTexCoord2f(1.0, 0.0);
+            glVertex3f(pos, 50, -pos);
+            glTexCoord2f(1.0, 1.0);
+            glVertex3f(pos, 0, -pos);
+            glTexCoord2f(0.0, 1.0);
+            glVertex3f(-pos, 0, -pos);
+            glDisable(GL_TEXTURE_2D);
         glEnd();
     glPopMatrix();
-*/
+/*
     glPushMatrix();
         glTranslatef(0.0, 0.0, -pos);
         glScalef(1.0, 0.5, 0.001);
@@ -72,9 +120,6 @@ void World::Draw() {
         glutSolidCube(size);
     glPopMatrix();
 
-
-
-
     glPushMatrix();
         glTranslatef(pos, 0.0, 0.0);
         glScalef(0.001, 0.5, 1.0);
@@ -85,44 +130,6 @@ void World::Draw() {
         glScalef(0.001, 0.5, 1.0);
         glutSolidCube(size);
     glPopMatrix();
+*/
 
-    for (it = Drones().begin(); it != Drones().end(); it++) {
-        (*it)->Move();
-        (*it)->Draw();
-    }
-
-    bit = Bullets().begin();
-    while ( bit != Bullets().end() ) {
-        (*bit)->Move();
-        (*bit)->Draw();
-        if ((*bit)->Size() > 50) bit = Bullets().erase(bit);
-        else ++bit;
-    }
-
-
-//    for (bit = Bullets().begin(); bit != Bullets().end(); bit++) {
-//        (*bit)->Move();
-//        (*bit)->Draw();
-
-
-    it = Drones().begin();
-    while( it != Drones().end() )
-    {
-        collision = false;
-        bit = Bullets().begin();
-        while ( bit != Bullets().end() ) {
-            if ( distance_squared((*it)->Posx(), (*it)->Posz(), (*bit)->Posx(), (*bit)->Posz()) <= pow((*it)->Size() + (*bit)->Size(), 2) ) {
-                Bullets().erase(bit);
-                it = Drones().erase(it);
-                collision = true;
-                break;
-            }
-            else
-            {
-                ++bit;
-            }
-        }
-        if (!collision) ++it;
-
-    }
 }
